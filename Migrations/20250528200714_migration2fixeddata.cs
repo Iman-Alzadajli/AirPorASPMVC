f9d9f160210dfd1ba03075af43b0b35902a999c5
Iman-Alzadajli <imoimo033113@gmail.com>
2025-05-29 00:07:27 +0400

    fixed data

---
+++ Migrations/20250528200714_migration2fixeddata.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirPorASPMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration2fixeddata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
